7ab1f8590fd2169bca574c5e91026859d6815a05
Jake Morgan <jake@jakemorgan.co.uk>
2023-05-12 02:23:17 -0700

    Adding script to repo

---
+++ cheekygit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Initialise a new Git repository
+##git init
+
+# Clone the repository
+git clone git@github.com:jakemorgangit/CheekyGit.git
+
+# Navigate into the cloned repository
+cd CheekyGit
+
+# Specify the file you want to modify
+file="naughty.txt"  # Replace with your file name
+
+# Add a whitespace character to the file
+echo " " >> $file
+
+# Stage and commit the change
+git add $file
+git commit -m "automated review: added whitespace"
+
+# Pull any new changes from the main branch
+git pull origin main
+
+# Remove the whitespace character from the file
+# Note: This assumes that the whitespace is at the end of the file
+sed -i '$ d' $file
+
+# Stage and commit the change
+git add $file
+git commit -m "automated review: removed whitespace"
+
+# Push changes to the main branch
+git push origin main
+
